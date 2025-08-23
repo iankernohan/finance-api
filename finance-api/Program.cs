db18afb9c26e508b01e1c4cfea4482e3d952c0aa
Ian Kernohan <kernohani1@gmail.com>
2025-08-23 18:38:13 -0400

    rename dockerfile

--- finance-api/Program.cs
+++ finance-api/Program.cs
@@ -7,8 +7,6 @@ using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
